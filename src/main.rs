b510a939854d5a2cd778db46b28b358a8b855c1d
Brian Rowe <briprowe@gmail.com>
2018-11-28 18:30:31 -0500

    Write CSV file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(transpose_result)]
+
 use std::error::Error;
 use std::io::prelude::*;
 use std::{fmt, io, process};
@@ -93,31 +95,101 @@ impl<'a> fmt::Display for EdnPrinter<'a> {
                     })?;
                 write!(f, "{}", "}")
             }
-            // edn::Value::Tagged(tag, value) => {
-            //     write!(f, "#{}", tag)?;
-            //     write!(f, " {}", EdnPrinter::from(value))
-            // }
-            _ => write!(f, "Nope!"),
+            edn::Value::Tagged(tag, value) => {
+                write!(f, "#{}", tag)?;
+                write!(f, " {}", EdnPrinter::from(value.as_ref()))
+            }
         }
     }
 }
 
+#[derive(Debug)]
+struct ParseError {
+    linenum: usize,
+    cause: edn::parser::Error,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} ({}, {}): {} ",
+            self.linenum, self.cause.lo, self.cause.hi, self.cause.message
+        )
+    }
+}
+
+impl Error for ParseError {
+    fn description(&self) -> &str {
+        "Error while reading EDN"
+    }
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
+    use std::collections::BTreeSet;
+
+    let mut records = vec![];
+    let mut columns: BTreeSet<String> = BTreeSet::new();
+
+    let stdin = io::stdin();
+    for (idx, line) in stdin.lock().lines().enumerate() {
+        let line = line?;
+        let mut parser = Parser::new(&line);
+
+        if let Some(edn) = parser.read().transpose().or_else(|e| {
+            Err(ParseError {
+                linenum: idx,
+                cause: e,
+            })
+        })? {
+            match edn {
+                edn::Value::Map(m) => {
+                    let keys: Vec<String> = m
+                        .keys()
+                        .filter_map(|key| match key {
+                            edn::Value::Keyword(k) => Some(k.clone()),
+                            _ => {
+                                eprintln!("Skipping non keyword key: {}", EdnPrinter::new(&key));
+                                None
+                            }
+                        })
+                        .collect();
+
+                    columns.extend(keys);
+                    records.push(m);
+                }
+                _ => eprintln!("Skipping non map on line {}", idx),
+            }
+        }
+    }
+
     let mut writer = csv::WriterBuilder::new()
         .delimiter(b'\t')
         .from_writer(io::stdout());
 
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line?;
-        let mut parser = Parser::new(&line);
-        if let Some(result) = parser.read() {
-            let edn = result.unwrap();
-            let printer = EdnPrinter::new(&edn);
-            println!("{}", printer);
-        }
+    for c in &columns {
+        writer.write_field(c)?;
     }
+    writer.write_record(None::<&[u8]>)?;
 
+    let columns: Vec<edn::Value> = columns
+        .iter()
+        .map(|k| edn::Value::Keyword(k.to_string()))
+        .collect();
+
+    for r in records {
+        for c in &columns {
+            if let Some(field) = r
+                .get(c)
+                .and_then(|f| Some(format!("{}", EdnPrinter::new(f))))
+            {
+                writer.write_field(field.as_bytes())?;
+            } else {
+                writer.write_field("")?;
+            }
+        }
+        writer.write_record(None::<&[u8]>)?;
+    }
     Ok(())
 }
 
